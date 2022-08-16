d36b134acebee564fb47486a69a89e5334ec3f9f
Stefan Meza <stefanmeza93@gmail.com>
2022-08-16 22:46:24 +0200

    Add info about primite data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -5,4 +5,20 @@ consto.log('jonas');
 console.log(23)
 
 let firstName = 'Jonas';
-console.log(firstName)
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
