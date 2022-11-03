28739d9fe2a24c0b9af9fb6c9bed40bc83aaa48c
Stefan Meza <stefanmeza93@gmail.com>
2022-11-03 22:04:00 +0100

    Add default parameter and first-class functions as well as higher-order funcions info

--- 10 - A Closer Look at Functions/script.js
+++ 10 - A Closer Look at Functions/script.js
@@ -1,35 +1,63 @@
 "use strict";
 
-// Default parametri: pre ES6 koristili bi short circuiting da dodelimo neke default vrednosti varijablama, medjutim sa ES6 sada to mozemo da cinimo dikretno kod parametara funkcije stavljajuci operator =
-// Dobra stvar kod ovih parametara je sto mozemo da koristimo bilo koji expression cak mozemo da koristimo i druge parametre ako su vec definisani, npr kod price-a mozemo da koristimo flightNum ili numPassengers posto su definisani ispred price-a,
-const bookings = [];
+/*
+///////////////////////////////////////
+// Default Parameters
 
+const bookings = [];
 const createBooking = function (
-  flightNum = 1,
+  flightNum,
   numPassengers = 1,
   price = 199 * numPassengers
 ) {
-  // ES5 nacin za default parametre
-  // flightNum = flightNum || 1;
-  // numPassengers = numPassengers || "N20";
-  // price = price || 29;
-
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
   const booking = {
     flightNum,
     numPassengers,
     price,
   };
-
+  console.log(booking);
   bookings.push(booking);
-  console.log(bookings);
 };
 
-createBooking("LH143");
-createBooking("LH143", 2, 800);
-createBooking("LH143", 2);
-createBooking("LH143", 5);
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
 
-// Prvi argument ce uvek da bude mapiran na prvi parametar, kao i drugi argument koji ce uvek biti mapiran na drugi parametar
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
 
-// Ono sto ne mozemo da radimo jeste da izostavimo npr srednji argument prilikom pozivanja funkcije, posto ih js stavlja redom, ali jedan zgodan trick za to jeste da za taj argument koji zelimo da izostavimo (tj zelimo npr da se ucita njegova default vrednost) mozemo da prosledimo na tom mestu undefined, to je isto kao da nismo prosledili nista za taj argument i bice ocitana njegova default vrednost!
-createBooking("LH123", undefined, 1000);
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
