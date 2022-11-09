ffa002a7912fd2dd760a24458b9e761820801ef6
Stefan Meza <stefan.meza@quantox.com>
2022-11-09 22:38:44 +0100

    Add first ajax call with xmlhttprequest

--- 16 - Asynchronous JavaScript_Promises, Async_Await and AJAX/script.js
+++ 16 - Asynchronous JavaScript_Promises, Async_Await and AJAX/script.js
@@ -4,3 +4,36 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    const html = `<article class="country">
+                    <img class="country__img" src="${data.flags.png}" />
+                    <div class="country__data">
+                        <h3 class="country__name">${data.name.common}</h3>
+                        <h4 class="country__region">${data.region}</h4>
+                        <p class="country__row"><span>👫</span>${
+                          data.population
+                        }</p>
+                        <p class="country__row"><span>🗣️</span>${
+                          data.languages.srp
+                        }</p>
+                        <p class="country__row"><span>💰</span>${
+                          country === "serbia"
+                            ? data.currencies.RSD.name
+                            : data.currencies.EUR.name
+                        }</p>
+                    </div>
+                </article>`;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = "1";
+  });
+};
+
+getCountryData("serbia");
+getCountryData("portugal");
