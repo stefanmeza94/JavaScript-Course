0b514372d5969213da8b255c10b860647293927c
Stefan Meza <stefan.meza@quantox.com>
2022-12-12 15:47:36 +0100

    Add constructor function and prototypes in js

--- 14 - Object-Oriented Programming (OOP) With JavaScript/script.js
+++ 14 - Object-Oriented Programming (OOP) With JavaScript/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //    ovo ne valjda za performanse koda
+  // this.calcAge = function() {
+  //     console.log(2037 - this.birthYear)
+  // }
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+
+const jay = "Jay";
+
+console.log(jack instanceof Person);
+console.log(jay instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
